f017d00739107f66f6f726c1e26232994b365695
Alin Andersen <alin@alin.space>
2021-02-11 23:13:06 +0100

    Adds UnityContainer extensions.

---
+++ source/FluentCommands.Unity/UnityContainerExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Unity;
+
+namespace FluentCommands.Unity
+{
+    /// <summary>
+    /// Extensions for <see cref="IUnityContainer"/>.
+    /// </summary>
+    public static class UnityContainerExtensions
+    {
+        /// <summary>
+        /// Register command implementation type to the execution group with the given container.
+        /// </summary>
+        /// <typeparam name="TFluentCommandImplementation">Type of the implementation of the <see cref="IFluentCommand"/>.</typeparam>
+        /// <param name="executionGroup">Execution group to register the command to.</param>
+        /// <param name="container">Container to register the command to.</param>
+        /// <param name="name">Optional command name.</param>
+        public static void RegisterCommandWithContainer<TFluentCommandImplementation>(
+            this IExecutionGroup executionGroup, 
+            IUnityContainer container, 
+            string name = null)
+            where TFluentCommandImplementation : IFluentCommand
+        {
+            container.RegisterType<TFluentCommandImplementation>(name, TypeLifetime.PerContainer);
+            var implementation = container.Resolve<TFluentCommandImplementation>(name);
+
+            container.RegisterInstance(name, executionGroup.Register(implementation), InstanceLifetime.PerContainer);
+        }
+
+        /// <summary>
+        /// Register command implementation type to the execution group with the given container.
+        /// </summary>
+        /// <typeparam name="TFluentCommandImplementation">Type of the implementation of the <see cref="IFluentCommand{TParameter}"/>.</typeparam>
+        /// <typeparam name="TCommandParameter">Type of the command parameter.</typeparam>
+        /// <param name="executionGroup">Execution group to register the command to.</param>
+        /// <param name="container">Container to register the command to.</param>
+        /// <param name="name">Optional command name.</param>
+        public static void RegisterCommandWithContainer<TFluentCommandImplementation, TCommandParameter>(
+              this IExecutionGroup executionGroup,
+              IUnityContainer container,
+              string name = null)
+              where TFluentCommandImplementation : IFluentCommand<TCommandParameter>
+        {
+            container.RegisterType<TFluentCommandImplementation>(name, TypeLifetime.PerContainer);
+            var implementation = container.Resolve<TFluentCommandImplementation>(name);
+
+            container.RegisterInstance(name, executionGroup.Register(implementation), InstanceLifetime.PerContainer);
+        }
+    }
+}
